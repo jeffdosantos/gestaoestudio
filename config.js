f29a8f1f4f661457db0b2755a3647e088697eb2c
jeffdosantos <jeff.fbds@gmail.com>
2026-05-08 17:51:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // 3) Use somente a chave publica anon/publishable. Nunca use a service_role no front-end.
 
 export const SUPABASE_URL = "https://isjhrccbvgznzecqhwom.supabase.co/rest/v1/";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzamhyY2Nidmd6bnplY3Fod29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNDg1NzQsImV4cCI6MjA5MzgyNDU3NH0.XTDwRZezy1fxrP52TNlexR1sGA0qZJzd5636o0Dk5K0";
+export const SUPABASE_ANON_KEY = "sb_publishable_xbUUw5xAwXfIz2WxXqZPzw_GUxOfhkc";
